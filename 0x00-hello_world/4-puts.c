6f1a426b2b9d8c50f90235a1310ca1c3877d1a57
Ashraf Atef Alx machine <alxashraf0@gmail.com>
2023-10-05 09:08:42 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/**
+* main function: prints "Programming is like building a multilingual puzzle 
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
