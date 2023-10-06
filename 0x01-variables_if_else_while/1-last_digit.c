dd15e6799ad78d8a974668ab673cc90d19789e67
Ashraf Atef Alx machine <alxashraf0@gmail.com>
2023-10-06 08:41:51 -0700

    task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print out if the number greater than or less than or equal 0
+ * Return: print out 0 if there is no error
+ * Ashraf Atef
+ */
+int main(void)
+{
+	int n;
+	int lastDigit = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	}
+	else if (n < 6)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	}
+	return (0);
+}
