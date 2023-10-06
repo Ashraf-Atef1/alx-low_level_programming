d6326f5e421534811f30d7069b23e7f5a574e295
Ashraf Atef Alx machine <alxashraf0@gmail.com>
2023-10-06 08:25:52 -0700

    task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print out if the number greater than or less than or equal 0
+ * Return: print out 0 if there is no error
+ * Ashraf Atef
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n>0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if(n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	else
+	{
+	printf("%d is negative\n", n);
+	}
+	return (0);
+}
