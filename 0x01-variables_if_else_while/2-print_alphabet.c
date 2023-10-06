2ef3ace6a467abbb5d880352879fcf50fcaec5ef
Ashraf Atef Alx machine <alxashraf0@gmail.com>
2023-10-06 09:02:41 -0700

    task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all leters from a to z and start a new line
+ * Return: print out 0 if there is no error
+ * Ashraf Atef
+ */
+int main(void)
+{
+	char letter;
+
+	for(letter = 'a'; letter < 123; letter++)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
