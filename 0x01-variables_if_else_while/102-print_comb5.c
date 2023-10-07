84433b8b90987f1ce032ed3ff2be364ef128920b
Ashraf Atef Alx machine <alxashraf0@gmail.com>
2023-10-07 05:14:52 -0700

    task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - print all compination of three numbers from 0 to 9 sparated by ","
+ * main - print all compination of four numbers from 0 to 9 sparated by ","
  * then start a new line
  * Return: print out 0 if there is no error
  * Ashraf Atef
  */
 int main(void)
 {
-	int num;
+	int num1;
 	int num2;
-	int num3;
-	int num4;
 
-	for (num = 48; num < 58; num++)
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = 48; num2 < 58; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (num3 = num; num3 < 58; num3++)
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (!(num1 == 98 && num2 == 99))
 			{
-				for (num4 = num2 + 1; num4 < 58; num4++)
-				{
-				putchar(num);
-				putchar(num2);
-				putchar(' ');
-				putchar(num3);
-				putchar(num4);
-				if (num == '9' && num2 == '8' && num3 == '9')
-				{
-				continue;
-				}
-				putchar(',');
-				putchar(' ');
-				}
+				putchar(','); // Print comma
+				putchar(' '); // Print space
 			}
 		}
 	}
