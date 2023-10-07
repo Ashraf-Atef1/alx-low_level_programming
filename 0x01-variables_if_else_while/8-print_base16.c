2c6b65d6bb8b3e5d8ec875884b8f0328ce93a78d
Ashraf Atef Alx machine <alxashraf0@gmail.com>
2023-10-06 17:45:16 -0700

    task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print all leters from a to z reversely and start a new line
+ * main - print all 16 hex characters then start a new line
  * Return: print out 0 if there is no error
  * Ashraf Atef
  */
@@ -9,13 +9,13 @@ int main(void)
 {
 	char letter;
 
-	for (letter = '0'; letter >= '9'; letter--)
+	for (letter = '0'; letter <= '9'; letter++)
 	{
-	putchar(letter);
+		putchar(letter);
 	}
-	for (letter = 'a'; letter >= 'f'; letter--)
+	for (letter = 'a'; letter <= 'f'; letter++)
 	{
-	putchar(letter);
+		putchar(letter);
 	}
 	putchar('\n');
 	return (0);
