3d68e1852515e9b106a17d0705355839bee43291
Ashraf Atef Alx machine <alxashraf0@gmail.com>
2023-10-07 04:20:15 -0700

    task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print all compination numbers from 0 to 9 sparated by "," then start a new line
+ * Return: print out 0 if there is no error
+ * Ashraf Atef
+ */
+int main(void)
+{
+	int num;
+	int num2;
+
+	for (num = 48; num < 58; num++)
+	{
+		for(num2 = num + 1; num2 < 58; num2++ )
+		putchar(num);
+		putchar(num2);
+		if (num != '8' && num2 != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
