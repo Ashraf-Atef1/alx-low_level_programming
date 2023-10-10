8558c58273a127068c3f361ebdf8647e7a6e8b53
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-10 20:22:17 +0300

    task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - return absolute number of the input
+ * @n: the number
+ * Return: the absolute number of the input
+ * Ashraf Atef
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
