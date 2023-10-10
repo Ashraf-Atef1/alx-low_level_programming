2cca8a0bb6c3d34acf27b14dd850eaf9f7dc8373
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-11 01:17:11 +0300

    task

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - finds and prints the sum of the even-valued terms
+ * starting with 1 and 2
+ * Return: 0
+ * Ashraf Atef
+ */
+
+int main(void)
+{
+	unsigned long long first;
+	unsigned long long second;
+	unsigned long long next;
+	unsigned long long sum;
+
+	first = 1;
+	second = 2;
+	next;
+	sum = 2;
+	while (1)
+	{
+		next = first + second;
+
+		if (next > 4000000)
+			break;
+
+		if (next % 2 == 0)
+			sum += next;
+
+		first = second;
+		second = next;
+	}
+
+	printf("%llu\n", sum);
+
+	return (0);
+}
