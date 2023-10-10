5da9593187b2039569786fbe9e6031839ee7fa55
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-11 00:14:23 +0300

    task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - prints the input times table, starting with 0
+ * @num: the number
+ * Ashraf Atef
+ */
+
+void print_times_table(int num)
+{
+	int i;
+	int j;
+	int n;
+
+	for (i = 0; i <= num; i++)
+	{
+		_putchar('0');
+		for (j = 1; j <= num; j++)
+		{
+			n = i * j;
+			printf(",%4d", n);
+		}
+		_putchar('\n');
+	}
+}
