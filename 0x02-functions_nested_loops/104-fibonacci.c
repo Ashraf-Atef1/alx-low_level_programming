f7435d050d56bac4ee0ed3aaa3edd6ee765c6645
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-11 02:30:14 +0300

    task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
-/**
- * main - prints the first 98 Fibonacci numbers
- * starting with 1 and 2
- * Return: 0
- * Ashraf Atef
- */
 
 int main(void)
 {
-	int n;
-	int i;
-	unsigned long first;
-	unsigned long second;
+	int n = 98; // Change this to the number of Fibonacci numbers you want to print
+	unsigned long first = 1;
+	unsigned long second = 2;
 	unsigned long next;
-	unsigned long upper;
-	unsigned long lower;
 
-	n = 98;
-	first = 1;
-	second = 2;
-	upper = 0;
-	lower = 0;
-	printf("%ld, %ld", first, second);
-	for (i = 3; i <= n; i++)
+	printf("%lu, %lu", first, second);
+
+	for (int i = 3; i <= n; i++)
 	{
 		next = first + second;
 		if (next >= first)
@@ -31,13 +18,16 @@ int main(void)
 		}
 		else
 		{
-			upper = (next >> 32);
-			lower = (next & 0xFFFFFFFF);
+			unsigned long upper = (next >> 32);
+			unsigned long lower = (next & 0xFFFFFFFF);
 			printf(", %lu%08lu", upper, lower);
 		}
+
 		first = second;
 		second = next;
 	}
+
 	printf("\n");
-	return (0);
+
+	return 0;
 }
