6707728b8ac396debb54b71c0c3e49bef462134a
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-10 20:01:17 +0300

    task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - check if the number is postive, 0 or negative
+ * and print the sign of the number
+ * @n: the number
+ * and create a new line 10 times
+ * Return: 1 if the number is postive, 0 if 0
+ * and -1 if negative
+ * Ashraf Atef
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-1');
+		return (-1);
+	}
+}
