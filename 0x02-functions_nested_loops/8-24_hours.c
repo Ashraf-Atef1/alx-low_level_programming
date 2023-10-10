a9b2828161ac70e919626bc097259e143276921e
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-10 20:51:26 +0300

    task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * jack_bauer - print all minutes in the day
+ * Ashraf Atef
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			if (h >= 10)
+			{
+				_putchar(h / 10 + '0');
+				_putchar(h % 10 + '0');
+			}
+			else
+			{
+				_putchar(h + '0');
+			}
+			_putchar(':');
+			if (m >= 10)
+			{
+				_putchar(m / 10 + '0');
+				_putchar(m % 10 + '0');
+			}
+			else
+			{
+				_putchar(m + '0');
+			}
+		}
+	}
+}
