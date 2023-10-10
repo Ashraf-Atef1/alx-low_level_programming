052c609f14370ff349a6c07ad049b34130631e4b
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-10 19:42:43 +0300

    task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print out all litters a-z
+ * and create a new line 10 times
+ * Return: 0
+ * Ashraf Atef
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
