078d119b4e7fa223704bd921f235f551d5363d74
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-10 20:33:31 +0300

    task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_last_digi - pritn and reaturn the last digit of the number
+ * print_last_digit - pritn and reaturn the last digit of the number
  * @n: the number
  * Return: the last digit of the number
  * Ashraf Atef
@@ -8,9 +8,9 @@
 
 int print_last_digit(int n)
 {
+	n %= 10;
 	if (n < 0)
 		n *= -1;
-	n %= 10;
 	_putchar(n + '0');
 	return (n);
 }
