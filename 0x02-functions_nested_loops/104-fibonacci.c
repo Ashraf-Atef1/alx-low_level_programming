9459a2977ec44b8b5c30dcacbdc740f3fba05acf
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-11 02:24:44 +0300

    task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,35 +13,30 @@ int main(void)
 	unsigned long first;
 	unsigned long second;
 	unsigned long next;
-	unsigned long first2;
-	unsigned long second2;
-	unsigned long next2;
+	unsigned long upper;
+	unsigned long lower;
 
 	n = 98;
 	first = 1;
 	second = 2;
-	first2 = 0;
-	second2 = 0;
+	upper = 0;
+	lower = 0;
 	printf("%ld, %ld", first, second);
 	for (i = 3; i <= n; i++)
 	{
-		if (((first * 10000000000 + first2) + (second * 10000000000 + second2)) < 10000000000)
+		next = first + second;
+		if (next >= first)
 		{
-			next = first + second;
 			printf(", %lu", next);
-			first = second;
-			second = next;
 		}
 		else
 		{
-			next = ((first * 10000000000 + first2) + (second * 10000000000 + second2)) / 10000000000;
-			next2 = ((first * 10000000000 + first2) + (second * 10000000000 + second2)) % 10000000000;
-			printf(", %lu%lu", next, next2);
-			first = second;
-			first2 = second2;
-			second = next;
-			second2 = next2;
+			upper = (next >> 32);
+			lower = (next & 0xFFFFFFFF);
+			printf(", %lu%08lu", upper, lower);
 		}
+		first = second;
+		second = next;
 	}
 	printf("\n");
 	return (0);
