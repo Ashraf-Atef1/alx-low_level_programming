76f33690c3f1dc19348560bcfd52ddf2f2a33245
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-10 23:11:52 +0300

    task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - calculate the sum of two numbers
+ * @n1: The first number
+ * @n2: The second number
+ * Return: the sum of the 2 input numbers
+ * Ashraf Atef
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
