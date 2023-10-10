71d2865dc85a390c255e731154a241b5d83ecc1e
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-10 23:59:03 +0300

    task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_to_98 - print all numbers to 98
+ * Ashraf Atef
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("98\n");
+}
