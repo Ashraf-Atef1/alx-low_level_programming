c46134a89aecb66d27669e7d2badbd934f6ff5c5
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-11 00:58:06 +0300

    task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints the first 50 Fibonacci numbers
- * starting with 1 and 2
+ * main - prints all natural number below 1024 that accept
+ * divided by 3 and 5
  * Return: 0
  * Ashraf Atef
  */
@@ -9,20 +9,14 @@
 int main(void)
 {
 	int i;
-	int n1;
-	int n2;
+	int max;
 	int sum;
 
-	n1 = 1;
-	n2 = 2;
-	printf("%d, %d", n1, n2);
-	for (i = 2; i <= 50; i++)
-	{
-		sum = n1 + n2;
-		printf(", %d", sum);
-		n1 = n2;
-		n2 = sum;
-	}
-	putchar('\n');
+	max = 1024;
+	sum = 0;
+	for (i = 1; i <= max; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	printf("%d\n", sum);
 	return (0);
 }
