c4a4299f82c46425c8b31079e0cf881d648f9a8e
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-11 05:38:05 +0300

    task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 /**
- * main - prints the first 98 Fibonacci numbers
- * starting with 1 and 2
- * Return: 0
+ * printMid - prints part of numbers
+ * @a: first number
+ * @b: second number
+ * @i: the counter
+ * @n: the counter stop
  * Ashraf Atef
  */
-
-int main(void)
+void printMid(unsigned long a, unsigned long b, unsigned int i, unsigned int n)
 {
-	unsigned long a;
-	unsigned long b;
-	unsigned long next;
-	unsigned int n;
-	unsigned int i;
 	unsigned long a1;
 	unsigned long b1;
 	unsigned long a2;
@@ -20,17 +16,6 @@ int main(void)
 	unsigned long next1;
 	unsigned long next2;
 
-	n = 98;
-	a = 1;
-	b = 2;
-	printf("%ld, %ld", a, b);
-	for (i = 3; i <= n - 6; i++)
-	{
-		next = a + b;
-		printf(", %lu", next);
-		a = b;
-		b = next;
-	}
 	a1 = a / 100000000000;
 	b1 = b / 100000000000;
 	a2 = a % 100000000000;
@@ -53,12 +38,40 @@ int main(void)
 		next2 = a2 + b2;
 		if (next1 % 10 == 0)
 			next1 /= 10;
-		printf(", %lu%lu%lu", next1 / 10, next1 % 10 + next2 / 100000000000, next2 % 100000000000);
+		printf(", %lu%lu%lu", next1 / 10,
+			   next1 % 10 + next2 / 100000000000, next2 % 100000000000);
 		a1 = b1;
 		a2 = b2;
 		b1 = next1;
 		b2 = next2;
 	}
+}
+/**
+ * main - prints the first 98 Fibonacci numbers
+ * starting with 1 and 2
+ * Return: 0
+ * Ashraf Atef
+ */
+int main(void)
+{
+	unsigned long a;
+	unsigned long b;
+	unsigned long next;
+	unsigned int n;
+	unsigned int i;
+
+	n = 98;
+	a = 1;
+	b = 2;
+	printf("%ld, %ld", a, b);
+	for (i = 3; i <= n - 6; i++)
+	{
+		next = a + b;
+		printf(", %lu", next);
+		a = b;
+		b = next;
+	}
+	printMid(a, b, i, n);
 
 	printf("\n");
 
