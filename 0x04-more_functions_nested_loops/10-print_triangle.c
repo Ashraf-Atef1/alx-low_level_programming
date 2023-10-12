d2520b5c48ed1681f77821b2f717529f2255959b
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-12 18:47:46 +0300

    task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,27 +7,27 @@
  */
 void print_triangle(int size)
 {
-    int i;
-    int j;
-    int n;
+	int i;
+	int j;
+	int n;
 
-    if (size > 0)
-    {
-        for (i = 1; i <= size; i++)
-        {
-            for (j = 0; j < size - i; j++)
-            {
-                _putchar(' ');
-            }
-            for (n = 0; n < i; n++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
-    else
-    {
-        _putchar('\n');
-    }
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (n = 0; n < i; n++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
