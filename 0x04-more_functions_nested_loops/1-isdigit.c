e6b9b74cbadaec595ffbbc30322c521e5ee8dcd1
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-12 16:56:39 +0300

    task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * _isupper - return 1 if c is a number and 0 if not
+ * @c: the letter to check
+ * Return: return 1 if a number and 0 if not
+ * Ashraf Atef
+ */
+int _isdigit(int c)
+{
+    if (c >= '0' && c <= '9')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
