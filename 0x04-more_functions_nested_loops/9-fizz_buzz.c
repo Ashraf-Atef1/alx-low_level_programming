f882fa1c98ecce511650d51852121e34166ebe53
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-12 18:27:15 +0300

    task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - The “Fizz-Buzz test” printer from 0 to 100
+ * Return: 0 if ture exit
+ * Ashraf Atef
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0)
+			printf("Fizz");
+		if (i % 3 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+
+	return (0);
+}
