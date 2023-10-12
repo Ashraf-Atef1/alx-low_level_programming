786e0974807a3bbdc481d7054839eff52a5c151d
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-12 17:14:08 +0300

    task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isdigit - print all nums from 0 to 9
+ * print_numbers - print all nums from 0 to 9
  * Ashraf Atef
  */
 void print_numbers(void)
 {
-    int i;
+	int i;
 
-    for (i = '0'; i <= '9'; i++)
-        _putchar(i);
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
 }
