6f00c5b1c9a2de2c82ea9e0c1c5900e4136697b1
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-12 17:09:00 +0300

    task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply and return the multi of 2 digits
+ * @a: first digit
+ * @b: the second digit
+ * Return: return the multi of 2 digits
+ * Ashraf Atef
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
