7b72daff5159c4318e6827f01b34feadb57ed8a9
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-12 19:44:54 +0300

    task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - print the input number by _putcahr()
+ * @n: the number
+ * Ashraf Atef
+ */
+void print_number(int n)
+{
+	if (n / 10 == 0)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
