bd897b589563cd70db6fd32b85bc111051a8461a
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-12 20:16:31 +0300

    task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,18 @@
  */
 void print_number(int n)
 {
+	unsigned int k = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n *= -1;
+		k = -n;
 	}
 
-	if (n / 10 != 0)
+	if (k / 10 != 0)
 	{
 		print_number(n / 10);
 	}
-
 	_putchar((n % 10) + '0');
 }
