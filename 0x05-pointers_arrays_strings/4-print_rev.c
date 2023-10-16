79f67b5680d2bb26b0c352c96f1bc0c10732a0b0
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-16 16:59:56 +0300

    task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - take string pointer then print it in reverse order
+ * follwed by a new line
+ * @s: pointer for a string
+ * Ashraf Atef
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (s[i] == *s)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
