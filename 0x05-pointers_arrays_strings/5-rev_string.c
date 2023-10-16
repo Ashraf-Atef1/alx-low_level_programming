03613a36b09194c26cb8267a1de6951ba99d8493
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-16 17:26:54 +0300

    task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - take string pointer then retern it in a reverse order
+ * follwed by a new line
+ * @s: pointer for a string
+ * Ashraf Atef
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	char str[i];
+
+	while (i > 0)
+	{
+		i--;
+		str[j] = s[i];
+		j++;
+	}
+	_putchar('\n');
+}
