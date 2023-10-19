b69cc89bcec9deffeeeeb9e11168f036599e4cdd
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-19 20:16:48 +0300

    task

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,60 +2,48 @@
 #include <stdio.h>
 
 /**
- * print_line - Prints a line of the buffer as specified
- * @c: The buffer to print
- * @s: Number of bytes to print
- * @l: Line number
- * Ashraf Atef
- */
-
-void print_line(char *c, int s, int l)
-{
-	int j, k;
-
-	printf("%08x: ", l * 10);
-
-	for (j = 0; j <= 9; j++)
-	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
-	}
-
-	putchar(' ');
-
-	for (k = 0; k <= s; k++)
-	{
-		char ch = c[l * 10 + k];
-
-		if (ch >= 32 && ch <= 126)
-			putchar(ch);
-		else
-			putchar('.');
-	}
-
-	putchar('\n');
-}
-
-/**
  * print_buffer - Prints a buffer as specified
  * @b: The buffer to print
  * @size: The number of bytes to print
+ * Ashraf Atef
  */
 void print_buffer(char *b, int size)
 {
-	int i, lines = (size - 1) / 10;
-
-	for (i = 0; i <= lines && size > 0; i++)
-	{
-		int bytes = (i < lines) ? 9 : (size % 10) - 1;
-
-		print_line(b, bytes, i);
-	}
+	int i;
+	int j;
 
 	if (size <= 0)
-		putchar('\n');
+	{
+		printf("\n");
+		return;
+	}
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+
+				printf("%02x", b[i + j]);
+			else
+				printf("  ");
+			if (j % 2 == 1)
+				printf(" ");
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				if (b[i + j] >= 32 && b[i + j] <= 126)
+					printf("%c", b[i + j]);
+				else
+					printf(".");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
 }
