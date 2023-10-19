860447a6ab12dd908ed02bd707f0358a5e0c8c1b
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-19 13:43:43 +0300

    task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,19 +6,25 @@
  */
 void print_number(int n)
 {
+	unsigned int num = 0;
+
 	if (n < 0)
 	{
-		n = -n;
+		num = n * -1;
 		_putchar('-');
-	}
-
-	if (n / 10 == 0)
-	{
-		_putchar(n + '0');
 	}
 	else
 	{
-		print_number(n / 10);
-		_putchar(n % 10 + '0');
+
+		num = n;
+	}
+	if (num / 10 == 0)
+	{
+		_putchar(num + '0');
+	}
+	else
+	{
+		print_number(num / 10);
+		_putchar(num % 10 + '0');
 	}
 }
