b151ad6d5ac50ce19d69d55c549d67419e0279b8
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-19 20:08:31 +0300

    task

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,14 +11,10 @@ void print_buffer(char *b, int size)
 {
 	int i, j;
 
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x: ", i);
+
 		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size)
@@ -29,20 +25,27 @@ void print_buffer(char *b, int size)
 			if (j % 2)
 				printf(" ");
 		}
+
 		printf(" ");
+
 		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size)
 			{
 				char c = b[i + j];
-
-				printf("%c", (c >= 32 && c <= 126) ? c : '.');
+				putchar(c >= 32 && c <= 126 ? c : '.');
 			}
 			else
 			{
-				printf(" ");
+				putchar(' ');
 			}
 		}
+
+		printf("\n");
+	}
+
+	if (size <= 0)
+	{
 		printf("\n");
 	}
 }
