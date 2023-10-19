942f2a724a31fc1e457f312a266a4d2c7ad21662
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-19 13:17:25 +0300

    task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,7 +17,11 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = src[j];
 		j++;
 	}
-	if (src[j] == '\0' && j)
-		dest[j] = src[j];
+
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 	return (dest);
 }
