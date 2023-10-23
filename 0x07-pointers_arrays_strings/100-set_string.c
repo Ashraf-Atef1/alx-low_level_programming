df08b0b932dcca609ba3d0ceed8be3de83b51374
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-23 19:28:29 +0300

    task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - make s point to to
+ * @s: make it point it s
+ * @to: adress to point to
+ * Ashraf Atef
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
