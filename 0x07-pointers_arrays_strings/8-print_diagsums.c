110967aec89ab67678bd64cc693a8fbb9bac32df
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-23 18:01:13 +0300

    task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - print the sum of diagsums of the square array
  * @a: the array
@@ -9,16 +10,16 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
+	int d1 = 0;
+	int d2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		_putchar((&a[i])[i]);
+		d1 += (&a[i])[i];
 	}
-	_putchar(',');
-	_putchar(' ');
-	for (; i >= 0; i++)
+	for (; i >= 0; i--)
 	{
-		_putchar((&a[i])[i]);
+		d2 += (&a[i])[i];
 	}
-	_putchar('\n');
+	printf("%d, %d\n", d1, d2);
 }
