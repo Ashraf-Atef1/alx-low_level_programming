7a4ec3f892cac62dae2172319fbe55bcb848ba21
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-25 15:02:16 +0300

    task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse order
+ * @s: pointer for the string
+ * Ashraf Atef
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+		if (*(s - 1) < 32)
+			_putchar('\n');
+	}
+}
