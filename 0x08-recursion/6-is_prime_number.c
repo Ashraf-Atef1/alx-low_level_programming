150143190a58019f3eaa8424c18d01711ac099c0
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-25 16:24:50 +0300

    task

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int _get_sqrt(int i, int n);
+
+/**
+ * _sqrt_recursion - return sqroot of n
+ * @n: the number
+ * Return: return the sqroot of n
+ * Ashraf Atef
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_get_sqrt(0, n));
+}
+
+/**
+ * _get_sqrt - return sqroot of n
+ * @i: the result
+ * @n: the number
+ * Return: return the sqroot of n
+ * Ashraf Atef
+ */
+int _get_sqrt(int i, int n)
+{
+	if (i)
+		return (_get_sqrt(i + 1, n));
+	if (i * i > n)
+		return (-1);
+	return (i);
+}
