61ba6a84394a05f4d399270d3015ae997450ef87
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-27 13:25:38 +0200

    task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments
+ * @argc: the number of arguments
+ * Return: return 0 if programe exit in a right way
+ * Ashraf Atef
+ */
+
+int main(int argc)
+{
+	printf("%s\n", --argc);
+	return (0);
+}
