81505702ea4e05131fe861ba1ee2129f60cf8da2
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-27 13:29:33 +0200

    task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,12 +3,14 @@
 /**
  * main - print the number of arguments
  * @argc: the number of arguments
+ * @argv: the array of arguments
  * Return: return 0 if programe exit in a right way
  * Ashraf Atef
  */
 
-int main(int argc)
+int main(int argc, char const *argv[])
 {
+	argv[0] = argv[0];
 	printf("%d\n", --argc);
 	return (0);
 }
