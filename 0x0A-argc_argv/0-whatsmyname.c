6b8e1a5893e085f3478cbebcfb6d3011038a3c69
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-27 13:19:05 +0200

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the programe name
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: return 0 if programe exit in a right way
+ * Ashraf Atef
+ */
+
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
