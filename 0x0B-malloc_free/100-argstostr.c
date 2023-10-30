04834c59efd45f5951725626e58ba6f04758da0e
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-30 20:01:33 +0200

    task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * argstostr - create a string from inputed arguments
+ * @ac: Number of arguments
+ * @av: An array of arguments
+ * Return: The allocated string location
+ * Ashraf Atef
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int size = 0;
+	int i = 0;
+	int j = 0;
+	char *str;
+
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j]; j++)
+			size++;
+
+	str = (char *)malloc(size + ac);
+	if (!str)
+		return (NULL);
+	size = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			str[size++] = av[i][j];
+		str[size++] = '\n';
+	}
+	str[size] = '\0';
+	return (str);
+}
