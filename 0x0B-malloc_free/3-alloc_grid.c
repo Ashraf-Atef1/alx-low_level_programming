6aab39c49f7f616e75fd2b48bb27ee31693d2801
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-30 17:35:52 +0200

    task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		p[i] = (int *)malloc(sizeof(int) * width);
+		p[i] = (int *)malloc(sizeof(int *) * width);
 		if (!p[i])
 		{
 			free(p);
