4f1c1c4411684107e64ef8deb32ba93d95eadba8
Ashraf Atef <ashraf.programer@gmail.com>
2023-10-30 15:09:30 +0200

    task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - create an array of chars in a specific size
+ * @size: of array
+ * @c: chars of array
+ * Return: a pointer for the array
+ * Ashraf Atef
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p = malloc(size);
+
+	if (size == 0)
+		return ('\0');
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+
+	return (p);
+}
