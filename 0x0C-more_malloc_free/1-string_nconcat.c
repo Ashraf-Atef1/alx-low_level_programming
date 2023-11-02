23ead7ff11ddfc4ae40ae9f409145721fc878f61
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-02 18:39:10 +0200

    task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,6 +13,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int size = 0, i = 0, sl1 = 0;
 	char *p;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i];)
 		i++;
 	size = i + n;
