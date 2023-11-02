086862cab2c1e3e6978688c5cbd57476db180537
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-02 20:58:28 +0200

    task

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _realloc - allocated new size
+ * @ptr: old allocated size
+ * @old_size: old size
+ * @new_size: new size
+ * Return: array pointer
+ * Ashraf Atef
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i = 0;
+	char *p, *p2 = ptr;
+
+	if (old_size > new_size)
+		return (NULL);
+	if (old_size == new_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+	for (i = 0; i < old_size; i++)
+		p[i] = p2[i];
+	return (p);
+}
