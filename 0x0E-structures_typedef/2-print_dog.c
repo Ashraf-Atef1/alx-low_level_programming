e05b48e392b105fe0471d2c68a7c08388e21106c
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-06 17:35:13 +0200

    task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,19 +8,17 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d)
-	{
-		if (d->name)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: nil");
-		if (d->name)
-			printf("Age: %f\n", d->age);
-		else
-			printf("Age: nil");
-		if (d->name)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: nil");
-	}
+
+	if (d->name)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: nil");
+	if (d->name)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: nil");
+	if (d->name)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: nil");
 }
