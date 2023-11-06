4ec018ede891fbf3ec6fdd483da58e1d7603b18a
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-06 16:19:25 +0200

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure of a dog object
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog onwer
+ * Ashraf Atef
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
