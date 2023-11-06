16bd53338d9d79dc900b8b273f6303a49eda6ab6
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-06 20:59:21 +0200

    task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,21 +11,67 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d = (dog_t *)malloc(sizeof(dog_t));
+	dog_t *new_dog;
+	char *name_copy, *owner_copy;
 
-	if (d && *name && age && *owner)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	name_copy = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (name_copy == NULL)
 	{
-		if (*name)
-			d->name = name;
-		if (age)
-			d->age = age;
-		if (*owner)
-			d->owner = owner;
-	}
-	else
-	{
-		free(d);
+		free(new_dog);
 		return (NULL);
 	}
-	return (d);
+
+	owner_copy = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (owner_copy == NULL)
+	{
+		free(new_dog);
+		free(name_copy);
+		return (NULL);
+	}
+
+	_strcpy(name_copy, name);
+	_strcpy(owner_copy, owner);
+
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
+
+	return (new_dog);
+}
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: input string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ */
+void _strcpy(char *dest, char *src)
+{
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
 }
