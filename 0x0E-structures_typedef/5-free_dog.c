cfe4391412541543710a4b960a0516f9e63ad1d7
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-06 20:39:36 +0200

    task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - remove struct dog from the heap
+ * @d: the struct pointer
+ * Ashraf Atef
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+		free(d);
+}
