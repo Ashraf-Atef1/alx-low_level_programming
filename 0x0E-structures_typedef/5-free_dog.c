15491c9e53371033614a5a05ba0189a0655b0eb8
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-06 20:54:30 +0200

    task

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -8,14 +8,14 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		if (d->name)
-			free(d->name);
-		if (d->age)
-			free(&(d->age));
-		if (d->owner)
-			free(d->owner);
-		free(d);
-	}
+	if (d == NULL)
+		return;
+
+	if (d->name != NULL)
+		free(d->name);
+
+	if (d->owner != NULL)
+		free(d->owner);
+
+	free(d);
 }
