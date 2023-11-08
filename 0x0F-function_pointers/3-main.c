b1402a217c19f23744345d36a186ae08a176aaf2
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-08 16:57:22 +0200

    task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - calculate 2 input numbers
+ * @argc: the number of arguments
+ * @argv: an array of arugments
+ * Return: 0 if exit in a right way
+ */
+int main(int argc, char const *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!get_op_func((char *)argv[2]))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] != '/' || argv[2][0] != '%') && argv[3][0] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func((char *)argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two intgers
+ * @a: first number
+ * @b: second number
+ * Return: the sum
+ * Ashraf Atef
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub two intgers
+ * @a: first number
+ * @b: second number
+ * Return: the sum
+ * Ashraf Atef
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply two intgers
+ * @a: first number
+ * @b: second number
+ * Return: the sum
+ * Ashraf Atef
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two intgers
+ * @a: first number
+ * @b: second number
+ * Return: the sum
+ * Ashraf Atef
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod two intgers
+ * @a: first number
+ * @b: second number
+ * Return: the sum
+ * Ashraf Atef
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
