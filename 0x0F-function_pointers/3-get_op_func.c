a4ba6a46f8ccfbc5e5ff5eacfdcc5fb3ee653e1e
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-08 17:00:48 +0200

    task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
@@ -20,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op)
 	{
-		if (s[0] == ops[i].op[0])
+		if (!strcmp(s, ops[i].op))
 			return (ops[i].f);
 		i++;
 	}
