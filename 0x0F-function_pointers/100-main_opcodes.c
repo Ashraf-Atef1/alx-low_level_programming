11d41b9919f434a3af021cf0b83ad3e635d48ea6
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-08 21:34:44 +0200

    task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -16,6 +16,7 @@ int main(int argc, char const *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
+		exit(1);
 		return (1);
 	}
 	int b_num = atoi(argv[1]);
@@ -23,6 +24,7 @@ int main(int argc, char const *argv[])
 	if (b_num < 0)
 	{
 		printf("Error\n");
+		exit(2);
 		return (2);
 	}
 
