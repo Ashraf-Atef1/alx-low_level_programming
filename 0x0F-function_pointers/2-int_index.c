cbf9d54c001c1b2cf65899055e0741896368a22d
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-08 15:41:58 +0200

    task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - excute a function with each array elemnts and return
+ * first index of right elemnt
+ * @array: the array of elemnts
+ * @size: the size of array
+ * @cmp : the function to excute
+ * Return: first indext of right element
+ * Ashraf Atef
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	else if (array && size && action)
+		for (i = 0; i < (int)size; i++)
+			if (cmp(array[i]))
+				return (i);
+	return (0);
+}
