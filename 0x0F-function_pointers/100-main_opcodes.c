4cae5dad46ed5669215107dbcc14011fee678bc6
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-08 19:58:06 +0200

    task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the first inserted number of bytes of the main function
+ * in hexadecimal code
+ * @argc: the number of arguments
+ * @argv: an array of arugments
+ * Return: 0 if exit in a right way
+ */
+int main(int argc, char const *argv[])
+{
+	int b_num = atoi(argv[1]);
+	int i = 0;
+	int (*f)(int, const char **) = main;
+	char *p = (char *)f;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (b_num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < b_num; i++)
+	{
+		printf("%x", p[i] & 0XFF);
+		if (i < b_num - 1)
+			putchar(' ');
+		else
+			putchar('\n');
+	}
+
+	return (0);
+}
