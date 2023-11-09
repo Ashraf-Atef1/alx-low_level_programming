daf03121bf9c50b24e5b1578a5473d0e42aca93a
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-09 21:51:51 +0200

    task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,7 +19,7 @@ void print_all(const char *const format, ...)
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(args, int));
+			printf("%c", va_arg(args, char));
 			break;
 		case 'i':
 			printf("%d", va_arg(args, int));
