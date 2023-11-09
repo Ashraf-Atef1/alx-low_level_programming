65612a661ec9ac9663ca54064976776c4813ca23
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-09 18:42:43 +0200

    task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print all numbers arguments follwed by a new line
+ * @separator: the separator between the numbers
+ * @n: number of the arguments
+ * Ashraf Atef
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i, x;
+	va_list array;
+
+	va_start(array, n);
+	for (i = 0; i < (int)n; i++)
+	{
+		x = va_arg(array, int);
+		printf("%d", x);
+		if (i < n - 1 && separator)
+			printf("%s", separator);
+	}
+	putchar('\n');
+	va_end(array);
+}
