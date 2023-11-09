d80af3114dfd05c2a0fbd457a6d1394e7e429810
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-09 18:19:45 +0200

    task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * sum_them_all - Print the sum of the arguments
+ * @n: number of the arugments
+ * Return: the sum of numbers
+ * Ashraf Atef
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, x;
+	int sum = 0;
+	va_list array;
+	va_start(array, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(array, int);
+		sum += x;
+	}
+	va_end(array);
+	return (sum);
+}
