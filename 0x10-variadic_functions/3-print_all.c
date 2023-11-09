ce6f10609b780ced729ef7138af287083e094306
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-09 19:42:32 +0200

    task

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - print all arguments follwed by a new line
+ * @format: specifyers in a string
+ * Ashraf Atef
+ */
+void print_all(const char *const format, ...)
+{
+	va_list args;
+	int i = 0;
+
+	va_start(args, format);
+	while (format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 's':
+			printf("%s", va_arg(args, char *));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		}
+
+		if ((format[i + 1] != '\0') && (format[i] == 'c' || format[i] == 'i' ||
+										format[i] == 's' || format[i] == 'f'))
+			printf(", ");
+
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
