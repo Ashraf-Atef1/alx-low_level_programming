5a784eac3e56d052b2a24b5638450c47119d69ff
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-16 20:17:37 +0200

    task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include "lists.h"
+void print_num(int n);
 /**
  * print_list - calculate the number of nodes
  * @h: pointer for a node in the linked list
@@ -14,12 +15,37 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str)
+		{
+			write(1, "[", 1);
+			print_num(h->len);
+			write(1, "] ", 2);
 			write(1, h->str, h->len);
+		}
 		else
+		{
+			write(1, "[", 1);
+			print_num(h->len);
+			write(1, "] ", 2);
 			write(1, "(nil)", 5);
+		}
 		write(1, "\n", 1);
 		h = h->next;
 		n++;
 	}
 	return (n);
+}
+
+void print_num(int n)
+{
+	char c_num = n % 10 + '0';
+
+	if (n / 10 == 0)
+	{
+		write(1, &c_num, 1);
+	}
+	else
+	{
+		print_num(n / 10);
+		write(1, &c_num, 1);
+	}
 }
