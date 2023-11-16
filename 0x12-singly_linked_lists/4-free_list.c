21d05d4a6e5de22b56b6e2255f9601b7b5725fb2
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-17 01:25:17 +0200

    task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - free node from a node list
+ * @head: pointer for the first node in the linked list
+ * Ashraf Atef
+ */
+
+void free_list(list_t *head)
+{
+	while (head)
+	{
+		list_t *cr_head = head;
+
+		head = head->next;
+		free(cr_head);
+	}
+}
