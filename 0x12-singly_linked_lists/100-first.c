fa7eb310c8fca8eff5677fcd1ef0d7efae9273ed
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-17 01:45:44 +0200

    task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+void b_main(void) __attribute__((constructor));
+/**
+ * b_main - free node from a node list
+ * Ashraf Atef
+ */
+
+void b_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
