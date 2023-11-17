6a584c5375ad966e7411387e7874b21994d6b054
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-17 02:18:24 +0200

    task

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,31 @@
+section .data
+    hello db "Hello, Holberton", 10, 0  ; 10 is the ASCII code for newline
+    format db "%s", 0
+
+section .text
+    global main
+
+main:
+    ; Set up arguments for printf
+    mov rdi, format
+    mov rsi, hello
+
+    ; Call printf
+    mov rax, 0   ; syscall number for sys_write
+    call printf
+
+    ; Exit the program
+    mov rax, 60  ; syscall number for sys_exit
+    xor rdi, rdi ; status 0
+    syscall
+
+section .text
+    ; Dummy printf function for assembly
+    global printf
+    printf:
+        ; Set up syscall parameters
+        mov rax, 1          ; syscall number for sys_write
+        mov rdi, 1          ; file descriptor 1 (stdout)
+        mov rdx, 18         ; message length
+        syscall
+        ret
