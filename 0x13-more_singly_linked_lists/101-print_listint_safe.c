ea692575766b4d5ed8244fdc26c98f53edb12a8e
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-21 06:29:27 +0200

    task

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "lists.h"
+/**
+ * print_listint_safe - calculate the number of nodes and print them
+ * @head: pointer for a node in the linked list
+ * Return: number of nodes
+ * Ashraf Atef
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i;
+	size_t n = 0;
+	const listint_t *node_list[100];
+
+	while (head)
+	{
+		for (i = 0; i <= n; i++)
+			if (node_list[i] == head)
+			{
+				for (i = 0; i < n; i++)
+					free((listint_t *)node_list[i]);
+				return (n);
+			}
+		node_list[n] = head;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		n++;
+	}
+	return (n);
+}
