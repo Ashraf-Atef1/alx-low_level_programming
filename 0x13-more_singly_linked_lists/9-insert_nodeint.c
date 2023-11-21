9100abb3df988ef84ba165c8a38eb0cd35c3aae8
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-21 04:58:20 +0200

    task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,6 +32,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_head->next = new_node;
 		return (new_node);
 	}
+	else if (i == idx)
+	{
+		new_head = new_node;
+		return (new_node);
+	}
 	free(new_node);
 	return (NULL);
 }
