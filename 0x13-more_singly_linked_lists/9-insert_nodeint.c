dadb8c49d1d01189e1fb1d6d43594959124e2acb
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-21 04:55:07 +0200

    task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	new_node->n = n;
-	while (new_head && ++i < idx)
+	while (new_head && idx && ++i < idx)
 	{
 		new_head = new_head->next;
 	}
