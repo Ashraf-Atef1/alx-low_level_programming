643480a95622f8b23ff1de905e86db680acea59a
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-23 17:33:07 +0200

    task

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * set_bit - set the bit in a specific postion
+ * @n: decimal number
+ * @index: the index of the bit
+ * Return: set value
+ * Ashraf Atef
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int j = 1;
+
+	while (index)
+	{
+		j = j << 1;
+		index--;
+	}
+	n = *n | j;
+	return (1);
+}
