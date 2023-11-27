2a31eabc3756c9836da0d235174043847ef5fccc
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-27 14:18:47 +0200

    task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,16 @@
 int create_file(const char *filename, char *text_content)
 {
 	int i = 0;
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	int fd = open(filename, O_WRONLY | O_TRUNC);
 
 	if (filename == NULL)
 		return (-1);
 	if (fd == -1)
-		return (-1);
+	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		if (fd == -1)
+			return (-1);
+	}
 	if (text_content[i])
 	{
 		while (text_content[i++])
