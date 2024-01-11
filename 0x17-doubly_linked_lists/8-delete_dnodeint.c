e7f347439bcaedc293e0b3edd9419d07f332cc05
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-11 17:58:09 +0200

    task

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete node at the specific index
+ * @head: the head for the linked list
+ * @index: the index of the node to delete
+ * Return: 1 if deleted or -1 if there an error
+ * Ashraf Atef
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *delNode = get_dnodeint_at_index(*head, index);
+
+	if (delNode)
+		if (!index)
+			if (delNode->next)
+				*head = delNode->next, delNode->next->prev = NULL;
+			else
+				*head = delNode->next;
+		else
+			delNode->prev->next = delNode->next, delNode->next->prev = delNode->prev;
+	else
+		return (-1);
+	free(delNode);
+	return (1);
+}
+
+/**
+ * get_dnodeint_at_index - get node at specific index
+ * @head: the head node pointer
+ * @index: the index of the node to get
+ * Return: the required node or Null if the node didn't exsist
+ * Ashraf Atef
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (i++ < index)
+		if (head)
+			head = head->next;
+		else
+			return (head);
+	return (head);
+}
