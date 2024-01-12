50ae77d95b1ab0f9a5061ede6b94a85936de633f
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-12 02:37:56 +0200

    task

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - keygen generates a key for the user
+ * @argc: the number of the arguments
+ * @argv: the list of the arguments
+ * Return: 0 if succesed or 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	char pass[7], *uName = argv[1], *codex;
+	int uLen = strlen(uName), i = 0, tmp = 0;
+
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	if (argc != 2)
+		return (1);
+	pass[0] = codex[(uLen ^ 0x3b) & 0x3f];
+	for (i = 0; i < uLen; i++)
+		tmp += uName[i];
+	pass[1] = codex[(tmp ^ 0x4f) & 0x3f];
+	for (i = 0, tmp = 1; i < uLen; i++)
+		tmp *= uName[i];
+	pass[2] = codex[(tmp ^ 0x55) & 0x3f];
+	for (i = 0, tmp = 1; i < uLen; i++)
+		if (tmp < uName[i])
+			tmp = uName[i];
+	srand(tmp ^ 0xe);
+	pass[3] = codex[rand() & 0x3f];
+	for (i = 0, tmp = 0; i < uLen; i++)
+		tmp += uName[i] * uName[i];
+	pass[4] = codex[(tmp ^ 0xef) & 0x3f];
+	for (i = 0, tmp = 0; i < *uName; i++)
+		tmp = rand();
+	pass[5] = codex[(tmp ^ 0xe5) & 0x3f];
+	pass[6] = '\0';
+	printf(pass);
+	return (0);
+}
--- 0x17-doubly_linked_lists/test.py
+++ 0x17-doubly_linked_lists/test.py
@@ -1,11 +1,18 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+n1 = 999
+n2 = 999
+res = 0
+
 def is_palindrome(number):
     str_number = str(number)
     return str_number == str_number[::-1]
-
-maxx = 0
-for i in range(100, 999, 1):
-    for j in range(100, 999, 1):
-        if is_palindrome(i * j) and i * j > maxx:
-            maxx = i * j
-with open('102-result', 'w') as file:
-    file.write(str(maxx))
+while n1 > 99:
+    while n2 > 99:
+        if is_palindrome(n1 * n2):
+            res = n1 * n2
+            print(res)
+            exit()
+        n2 -= 1
+    n2 = 999
+    n1 -= 1
