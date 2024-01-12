cda6c286e9249988ecddea68dd183786142a7bf5
Ashraf Atef <ashraf.programer@gmail.com>
2024-01-12 02:48:11 +0200

    task

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -10,35 +10,32 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned int i, b;
-	size_t len, add;
-	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	char p[7] = "      ";
+	char pass[7] = "", *uName = argv[1];
+	int uLen = strlen(uName), i = 0, tmp = 0;
+	char *codex =
+		"A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
 	if (argc != 2)
-	{
-		printf("Correct usage: ./keygen5 username\n");
 		return (1);
-	}
-	len = strlen(argv[1]);
-	p[0] = l[(len ^ 59) & 63];
-	for (i = 0, add = 0; i < len; i++)
-		add += argv[1][i];
-	p[1] = l[(add ^ 79) & 63];
-	for (i = 0, b = 1; i < len; i++)
-		b *= argv[1][i];
-	p[2] = l[(b ^ 85) & 63];
-	for (b = argv[1][0], i = 0; i < len; i++)
-		if ((char)b <= argv[1][i])
-			b = argv[1][i];
-	srand(b ^ 14);
-	p[3] = l[rand() & 63];
-	for (b = 0, i = 0; i < len; i++)
-		b += argv[1][i] * argv[1][i];
-	p[4] = l[(b ^ 239) & 63];
-	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
-		b = rand();
-	p[5] = l[(b ^ 229) & 63];
-	printf("%s\n", p);
+	pass[0] = codex[(uLen ^ 0x3b) & 0x3f];
+	for (i = 0; i < uLen; i++)
+		tmp += uName[i];
+	pass[1] = codex[(tmp ^ 0x4f) & 0x3f];
+	for (i = 0, tmp = 1; i < uLen; i++)
+		tmp *= uName[i];
+	pass[2] = codex[(tmp ^ 0x55) & 0x3f];
+	for (i = 0, tmp = 1; i < uLen; i++)
+		if (tmp < uName[i])
+			tmp = uName[i];
+	srand(tmp ^ 0xe);
+	pass[3] = codex[rand() & 0x3f];
+	for (i = 0, tmp = 0; i < uLen; i++)
+		tmp += uName[i] * uName[i];
+	pass[4] = codex[(tmp ^ 0xef) & 0x3f];
+	for (i = 0, tmp = 0; i < *uName; i++)
+		tmp = rand();
+	pass[5] = codex[(tmp ^ 0xe5) & 0x3f];
+	pass[6] = '\0';
+	printf(pass);
 	return (0);
 }
