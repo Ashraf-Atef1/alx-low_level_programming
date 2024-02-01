b52408d9f1de77cccbcc90e11a61f3e94c40af5a
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-01 18:21:33 +0200

    task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,9 +6,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hs = malloc(sizeof(hash_table_t));
 	long unsigned int i;
 
+	if (!hs)
+		return (NULL);
 	hs->size = size;
 	hs->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
-	for (i = 0; i < hs->size; i++)
-		hs->array[i] = NULL;
+	if (!hs->array)
+		return (NULL);
 	return (hs);
 }
