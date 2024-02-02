8b88bc3ca32b87f09fa5b018495cddda340d1d1e
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-03 00:14:45 +0200

    task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,21 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the hash table.
- *
- * This function creates a hash table with a given size. If memory allocation
- * fails, it returns NULL.
- *
- * Return: A pointer to the newly created hash table, or NULL if it fails.
- *
- * Authors: AhMeDMaGDY28
- * School: ALX CO 1 BLENDED
+ * hash_table_create - create a new hash table
+ * @size: a hash table size
+ * Return: a hash table pointer
+ * Ashraf Atef
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	hash_node_t **array_h;
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (!hash_table)
+	hash_table_t *hs = malloc(sizeof(hash_table_t));
+
+	if (!hs)
 		return (NULL);
-	array_h = calloc(size, sizeof(hash_node_t *));
-	if (!array_h)
+	hs->size = size;
+	hs->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
+	if (!hs->array)
 		return (NULL);
-	hash_table->size = size;
-	hash_table->array = array_h;
-	return (hash_table);
+	return (hs);
 }
