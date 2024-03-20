af6087576133188e9afa87b03b786c3c62ee2cd8
Ashraf Atef <ashraf.programer@gmail.com>
2024-03-20 17:08:07 +0200

    task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,11 +7,14 @@
 def island_perimeter(grid):
     """Calculate the island perimeter"""
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    col = len(grid)
+    row = len(grid[i])
+    for i in range(col):
+        for j in range(row):
             if grid[i][j] == 1:
-                if i and grid[i - 1][j] == 0:
-                    perimeter += 2
-                if j and grid[i][j - 1] == 0:
-                    perimeter += 2
+                perimeter += 2
+                if i and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j and grid[i][j - 1] == 1:
+                    perimeter -= 2
     return perimeter
