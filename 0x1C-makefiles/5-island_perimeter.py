272b06b936aaf7ab80a9997b4c62911880b2693e
Ashraf Atef <ashraf.programer@gmail.com>
2024-03-20 16:40:48 +0200

    task

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter.py
+"""
+def island_perimeter(grid):
+    """Calculate the island perimeter"""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += count_cell_perimeter(grid, i, j)
+    return perimeter
+
+def count_cell_perimeter(grid, i, j):
+    """Calculate the perimeter for each cell"""
+    sum = 0
+    if not grid[i][j - 1]: sum += 1
+    if not grid[i][j + 1]: sum += 1
+    if not grid[i - 1][j]: sum += 1
+    if not grid[i + 1][j]: sum += 1
+    return sum
