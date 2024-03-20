8d70275a18cc735ff54810842f9cbd5e3313e741
Ashraf Atef <ashraf.programer@gmail.com>
2024-03-20 16:42:28 +0200

    task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,6 +2,8 @@
 """
 5-island_perimeter.py
 """
+
+
 def island_perimeter(grid):
     """Calculate the island perimeter"""
     perimeter = 0
@@ -11,11 +13,16 @@ def island_perimeter(grid):
                 perimeter += count_cell_perimeter(grid, i, j)
     return perimeter
 
+
 def count_cell_perimeter(grid, i, j):
     """Calculate the perimeter for each cell"""
     sum = 0
-    if not grid[i][j - 1]: sum += 1
-    if not grid[i][j + 1]: sum += 1
-    if not grid[i - 1][j]: sum += 1
-    if not grid[i + 1][j]: sum += 1
+    if not grid[i][j - 1]:
+        sum += 1
+    if not grid[i][j + 1]:
+        sum += 1
+    if not grid[i - 1][j]:
+        sum += 1
+    if not grid[i + 1][j]:
+        sum += 1
     return sum
