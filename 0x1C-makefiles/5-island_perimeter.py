715e001623456583e2cc6bdfac095d3743d98548
Ashraf Atef <ashraf.programer@gmail.com>
2024-03-20 16:46:53 +0200

    task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,21 +5,24 @@
 
 
 def island_perimeter(grid):
-    """
-    Calculate the perimeter of the island described in grid.
-    """
+    """Calculate the island perimeter"""
     perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4  # Count all 4 sides of land cell
-                # Check adjacent cells (up, down, left, right)
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2 
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if i and j and grid[i][j] == 1:
+                perimeter += count_cell_perimeter(grid, i, j)
     return perimeter
+
+
+def count_cell_perimeter(grid, i, j):
+    """Calculate the perimeter for each cell"""
+    sum = 0
+    if not grid[i][j - 1]:
+        sum += 1
+    if not grid[i][j + 1]:
+        sum += 1
+    if not grid[i - 1][j]:
+        sum += 1
+    if not grid[i + 1][j]:
+        sum += 1
+    return sum
